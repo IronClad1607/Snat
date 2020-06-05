523c5fc442afabf95456c0d68c7982355de9bd8d
Ishaan Jain <ishaan160799@gmail.com>
2020-06-05 17:47:13 +0530

    layout reddy

--- app/src/main/java/com/ironclad/samplesnat/MainActivity.kt
+++ app/src/main/java/com/ironclad/samplesnat/MainActivity.kt
@@ -2,11 +2,16 @@ package com.ironclad.samplesnat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ironclad.snat.SnatToast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnToastShort.setOnClickListener {
+        }
     }
 }
