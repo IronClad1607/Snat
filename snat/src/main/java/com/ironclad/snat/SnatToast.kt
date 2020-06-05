cd19a021f177437da88e357dbc023f1c8f9219c3
Ishaan Jain <ishaan160799@gmail.com>
2020-06-05 17:49:07 +0530

    fun added into companion objects

--- snat/src/main/java/com/ironclad/snat/SnatToast.kt
+++ snat/src/main/java/com/ironclad/snat/SnatToast.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.widget.Toast
 
 class SnatToast {
-    public fun toastShort(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-    }
+    companion object{
+        public fun toastShort(context: Context, message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
 
-    public fun toastLong(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        public fun toastLong(context: Context, message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
     }
 }
